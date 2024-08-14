878fba9e21c127280652d76e00c5c4bdefaf4464
Didiana001 <musondakalobwe0@gmail.com>
2024-08-14 15:49:25 +0200

    closed

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,4 @@ urlpatterns = [
     path('', index, name='index'),  
     path('sense-data/', SenseDataList.as_view(), name='sense-data-list'),  # 
     path('sense-data-api/', SenseDataList.as_view(), name='sensor-data-list'),  
-    path('api/sense_data/create/', SenseDataCreate.as_view(), name='sense_data_create'),  #
+    path('api/sense_data/create/', SenseDataCreate.as_view(), name='sense_data_create'),  ]
